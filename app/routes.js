53b28cbd0a04dfdeee93597bd51cd44af0b7c73a
wicoo <william.cooper@nhs.net>
2022-04-06 16:03:07 +0100

    email and post pages

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,15 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/certificate-email-or-post', function (req, res) {
+  var contact = req.session.data['contact-group']
+
+  if (contact == "email"){
+    res.redirect('/email')
+  }
+  else {
+    res.redirect('/post')
+  }
+})
+
 module.exports = router;
