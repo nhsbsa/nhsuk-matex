a9a56522b75f485aca719745001a6074ba5b00d3
AbdulAfsal10 <abdul.afsal@nhs.net>
2023-06-26 15:46:42 +0100

    MVP 1.0

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // Add your routes here - above the module.exports line
-
-router.post('/certificate-email-or-post', function (req, res) {
-  var contact = req.session.data['contact-group']
+router.post(/certificate-email-or-post/, function (req, res) {
+  // creating a variable named contact - assigning the variable the value of the radio button selected
+  var contact = req.session.data['contact']
 
   if (contact == "email"){
     res.redirect('/email')
@@ -14,5 +14,21 @@ router.post('/certificate-email-or-post', function (req, res) {
     res.redirect('/post')
   }
 })
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
