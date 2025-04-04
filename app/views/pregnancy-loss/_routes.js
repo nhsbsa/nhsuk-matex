9b0626c143b826570e995383ef083f761245fca2
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2025-04-04 14:56:38 +0100

    Tidying up the templates across the project.

---
+++ app/views/pregnancy-loss/_routes.js
@@ -0,0 +1,19 @@
+// External dependencies
+const express = require('express');
+const router = express.Router();
+
+
+// Add your routes here - above the module.exports line
+router.post(/certificate-email-or-post/,  function (req, res) {
+  // creating a variable named contact - assigning the variable the value of the radio button selected
+  var contactGroup = req.session.data['contact-group']
+
+  if (contactGroup == "email"){
+    res.redirect('email')
+  }
+  else {
+    res.redirect('post')
+  }
+})
+
+module.exports = router;
